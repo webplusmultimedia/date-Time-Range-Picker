43b7eb9ebf99047f51ea83874d9988ee849c3562
webplusm <contact@webplusm.net>
2023-04-13 15:51:44 -0400

    fix : missing color changing

--- resources/views/components/datetime/arrow.blade.php
+++ resources/views/components/datetime/arrow.blade.php
@@ -1,4 +1,4 @@
-<span {{ $attributes->class(['absolute h-7 w-7  border-amber-400 transition lg:border-opacity-30 lg:group-hover:border-opacity-100']) }}
+<span {{ $attributes->class(['absolute h-7 w-7  border-primary-datepicker-400 transition lg:border-opacity-30 lg:group-hover:border-opacity-100']) }}
     style="left: calc(50% - (1.75rem/2))"
       role="button"
 ></span>
